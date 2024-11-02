b9c80c67b7bb68417b38c7b8e4d4fffbd9c9eb92
Emmanuel Mmeah <emmameah@gmail.com>
2024-11-02 22:18:13 +0000

    learning

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Hello, what is your name? ")
+age = input("Please tell us your age: ")
+location = input("Where do you come from? ") 
+print(f"Hello, {name}! You are {age} years old, and it's great to know that your location is {location}.")
